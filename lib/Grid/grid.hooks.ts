53f30e70568fb49b0eb108630e2567a8d59e7d94
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-06-28 22:04:11 +0530

    Delay layout compute on resize

--- lib/Grid/grid.hooks.ts
+++ lib/Grid/grid.hooks.ts
@@ -1,26 +1,51 @@
 import type { GridConfigType } from 'lib/types';
-import { useEffect, useState, type RefObject } from 'react';
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+  type RefObject,
+} from 'react';
 
 export const useGridConfig = (
   config: GridConfigType,
   parent: RefObject<HTMLElement | null> | undefined
 ): GridConfigType => {
   const [gridConfig, updateGridConfig] = useState<GridConfigType>(config);
+  const resizeRequested = useRef<boolean>(false);
+  const resizeTimeout = useRef<NodeJS.Timeout | undefined>(undefined);
 
-  useEffect(() => {
-    if (parent) {
-      updateGridConfig((prev) => ({
-        ...prev,
-        containerWidth: parent.current?.clientWidth || prev.containerWidth,
-      }));
+  const resizeHandler = useCallback(() => {
+    if (!resizeRequested.current) {
+      resizeRequested.current = true;
+      requestAnimationFrame(() => {
+        resizeRequested.current = false;
 
-      window.addEventListener('resize', () => {
-        updateGridConfig((prev) => ({
-          ...prev,
-          containerWidth: parent.current?.clientWidth || prev.containerWidth,
-        }));
+        clearTimeout(resizeTimeout.current);
+
+        resizeTimeout.current = setTimeout(() => {
+          updateGridConfig((prev) => ({
+            ...prev,
+            containerWidth: parent?.current?.clientWidth || prev.containerWidth,
+          }));
+        }, 30);
       });
     }
+  }, []);
+
+  useEffect(() => {
+    if (!parent) {
+      return;
+    }
+
+    updateGridConfig((prev) => ({
+      ...prev,
+      containerWidth: parent.current?.clientWidth || prev.containerWidth,
+    }));
+
+    window.addEventListener('resize', resizeHandler);
+
+    return () => window.removeEventListener('resize', resizeHandler);
   }, []);
 
   return gridConfig;
