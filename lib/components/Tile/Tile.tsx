565c8a72f2461e36cad7aca6f1879440fea51c13
Nitin Sharma <48169425+nitinsharmacs@users.noreply.github.com>
2025-07-02 00:20:41 +0530

    Scale down when selected

--- lib/components/Tile/Tile.tsx
+++ lib/components/Tile/Tile.tsx
@@ -1,5 +1,17 @@
 import type { TileProps } from 'lib/components/Tile/types';
 import './tile.css';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+const getScaleFactor = (
+  width: number,
+  xOffset: number,
+  height: number,
+  yOffset: number
+): string => {
+  const x = ((width - xOffset) / width).toFixed(2);
+  const y = ((height - yOffset) / height).toFixed(2);
+  return `${x},${y}`;
+};
 
 const Tile = ({ tile, image }: TileProps) => {
   return (
@@ -13,7 +25,18 @@ const Tile = ({ tile, image }: TileProps) => {
         top: `${tile.top}px`,
       }}
     >
-      <figure>
+      <CheckCircleIcon className='tile-checkicon' />
+      <figure
+        className='tile-image active'
+        style={{
+          transform: `scale(${getScaleFactor(
+            tile.width,
+            20,
+            tile.height,
+            20
+          )})`,
+        }}
+      >
         <img src={image.imageurl} />
       </figure>
     </div>
